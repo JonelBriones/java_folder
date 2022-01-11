aaf345fa8bf9481a545e9556380a6c9db6cb4685
Jonel Briones <jonelbriones@Jonels-MBP.lan>
2022-01-10 22:34:09 -0800

    First commit

---
+++ greetings/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        
+        // HOW TO RUN JAVA IN TERMINAL
+// "javac" insert name of file "Test.java"
+// Will compile and make a new file
+// "java" insert file name without extension "Test" to run program
+        
+        System.out.println("Hello Wordl!");
+    }
+}
+
