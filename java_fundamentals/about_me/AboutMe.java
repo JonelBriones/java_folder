f46c69f2479302b9e757e9aa50ef3a9558aaa046
Jonel Briones <jonel.c.briones@gmail.com>
2022-01-10 22:42:30 -0800

    first java code "about me"

---
+++ java_fundamentals/about_me/AboutMe.java
@@ -0,0 +1,7 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("My name is Jonel Briones");
+        System.out.println("I am 21 years old");
+        System.out.println("My hometown is San Jose, CA");
+    }
+}
