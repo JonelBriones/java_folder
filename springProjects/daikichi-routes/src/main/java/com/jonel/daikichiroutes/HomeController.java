d42bbbe969b8327d9840546b7be148e0ae919d38
Jonel Briones <jonel.c.briones@gmail.com>
2022-01-25 11:48:52 -0800

    path variable

--- springProjects/daikichi-routes/src/main/java/com/jonel/daikichiroutes/HomeController.java
+++ springProjects/daikichi-routes/src/main/java/com/jonel/daikichiroutes/HomeController.java
@@ -1,5 +1,6 @@
 package com.jonel.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,4 +22,24 @@ public class HomeController {
 	public String tomorrow() {
 			return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}	
+	@RequestMapping(value="/travel", method=RequestMethod.GET)
+	public String travel() {
+		return "Choose a location in the URL after /";
+	}
+	@RequestMapping(value="/travel/{destination}", method=RequestMethod.GET)
+	public String travelTo(@PathVariable("destination") String destination) {
+			return "Congratulations! You will soon travel to " + destination + "!";
+	}
+	
+	// using a conditional statement with a integer parameter
+	@RequestMapping(value="lotto/{number}", method=RequestMethod.GET) 
+	public String lotto(@PathVariable("number") int number) {
+			if (number%2 == 0) {
+				return "You will take a grand journey in the near future, but be weary of tempting offers.";
+			}
+			else {
+				return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+			}
+			
+	}
 }
