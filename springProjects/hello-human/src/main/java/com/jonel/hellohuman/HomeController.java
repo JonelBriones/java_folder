642247298aef3f350a8a7c83131d2bc2781ff7a2
Jonel Briones <jonel.c.briones@gmail.com>
2022-01-25 10:27:07 -0800

    routing,query parameter

---
+++ springProjects/hello-human/src/main/java/com/jonel/hellohuman/HomeController.java
@@ -0,0 +1,21 @@
+package com.jonel.hellohuman;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping("")
+	public String Human() {
+		return "Hello Human";
+	}
+	@RequestMapping("/first_name") 
+	public String firstName(@RequestParam(value="name", required=false) String firstName) {
+		return "Hello " + firstName;
+	}
+	@RequestMapping("/full_name")
+	public String name(@RequestParam(value="name", required=false) String firstName,@RequestParam(value="last_name", required=false) String lastName) {
+		return "Hello " + firstName + " " + lastName;
+	}
+}
